ad42d30afb3dd88d10f77291efb20c4317ef723c
Ali Aslam <aliaslamnoushadj@gmail.com>
2025-10-01 11:57:21 +0530

    testimonials issue  resolved

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import WhyChooseUs from "./components/WhyChooseUs";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 import CTASection from "./components/CTASection";
 import Footer from "./components/Footer";
 // import { Helmet, HelmetProvider } from "react-helmet-async";
